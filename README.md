c3348d1c1b98850e768fcacc8a2358cef0400a46
Emanuel <emanuelm@citiqprepaid.co.za>
2021-09-09 21:49:31 +0200

    Selenium test

---
+++ README.md
@@ -0,0 +1,18 @@
+# README
+
+### Download Chrome driver to `bin/` folder in this project
+https://sites.google.com/chromium.org/driver/downloads
+
+
+### Contents of `logins.txt` file to login to Twitter.
+```
+username
+password
+```
+
+
+### To Run
+```
+pip install -r requirements.txt
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,52 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    URL = 'https://www.twitter.com/login'
+    driver = webdriver.Chrome(executable_path='./bin/chromedriver')
+    driver.get(URL)
+    assert "Twitter" in driver.title
+
+    with open('./logins.txt', 'r') as f:
+        user = f.readline()
+        passw = f.readline()
+
+        assert user and passw
+
+        login(user, passw, dr=driver)
+
+        try:
+            like_post(driver)
+        finally:
+            time.sleep(30)
+            driver.close()
+
+
+def login(user, passw, dr=None):
+    if dr is not None:
+        form = dr.find_element_by_tag_name('form')
+        username = form.find_element_by_xpath(
+            "//input[@name='session[username_or_email]']")
+        username.send_keys(f'{user}')
+        password = form.find_element_by_xpath(
+            "//input[@name='session[password]']")
+        password.send_keys(f'{passw}')
+        form.submit()
+
+def like_post(dr=None):
+    if dr is not None:
+        article0 = WebDriverWait(dr, 20).until(
+                EC.presence_of_element_located((By.TAG_NAME, 'article'))
+        )
+        like = article0.find_element_by_xpath("//div[@data-testid='like']")
+        like.click()
+
+
+if __name__ == '__main__':
+    main()
+
