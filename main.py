6e2a8431d404c9d7e87c78d6567d00da5ccbf397
Emanuel <emanuel.magxothwa@gmail.com>
2021-09-13 09:57:09 +0200

    Headless update

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def main():
-    URL = 'https://www.twitter.com/login'
-    driver = webdriver.Chrome(executable_path='./bin/chromedriver')
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path='./bin/chromedriver',
+                              options=chrome_options)
     driver.implicitly_wait(7)
+
+    URL = 'https://www.twitter.com/login'
     driver.get(URL)
     assert "Twitter" in driver.title
 
@@ -38,6 +44,7 @@ def login(user, passw, dr=None):
         password = form.find_element_by_xpath(
             "//input[@name='session[password]']")
         password.send_keys(f'{passw}', Keys.RETURN)
+        print("Logged in...")
 
 
 def like_post(dr=None):
@@ -45,8 +52,10 @@ def like_post(dr=None):
         article0 = WebDriverWait(dr, 20).until(
                 EC.presence_of_element_located((By.TAG_NAME, 'article'))
         )
+        print(article0.text)
         like = article0.find_element_by_xpath("//div[@data-testid='like']")
         like.click()
+        print("Liked the post...")
 
 
 if __name__ == '__main__':
